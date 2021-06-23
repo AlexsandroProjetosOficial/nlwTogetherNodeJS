e99bc3f0f87592f9c66f0b36101ac23d495a8d43
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2021-06-23 17:40:47 -0300

    Aula3.

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from 'express';
 import { CreateUserService } from '../services/CreateUserService';
-
 class CreateUserController {
-    async handle(req: Request, res: Response ) {
+    async handle(req: Request, res: Response) {
         const { name, email, admin } = req.body;
 
         const createUserService = new CreateUserService();
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express, { urlencoded } from 'express';
+import express, { NextFunction, Request, Response, urlencoded } from 'express';
+import 'express-async-errors';
 import 'reflect-metadata';
 import './database';
 import { router } from './routes';
@@ -7,7 +8,18 @@ const app = express();
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-
 app.use(router);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    if(error instanceof Error) {
+        return res.status(400).json({
+            error: error.message
+        });
+    }
+
+    return res.status(500).json({
+        status: 'Error',
+        message: 'Internal Server Error.'
+    });
+});
 
 app.listen(3000, () => console.log('Server is running'));
